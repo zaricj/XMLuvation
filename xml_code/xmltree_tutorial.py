db9712023cc6cac281343a34296a408fbccf748a
zaricj <jovanzaric98@hotmail.com>
2024-02-20 11:26:56 +0100

    Change

--- xml_code/xmltree_tutorial.py
+++ xml_code/xmltree_tutorial.py
@@ -1,5 +1,19 @@
 import csv
 import xml.etree.ElementTree as ET
 
-xml_file = "Data.xml"
+datafile = "Data.xml"
 
+tree = ET.parse(datafile)
+root = tree.getroot()
+
+root_tag = root.tag
+print(root_tag)
+root_attribute = root.attrib
+print(root_attribute)
+
+for child in root:
+    print(child.tag, child.attrib)
+    for sibling in child:
+        print(sibling.tag, sibling.attrib)
+        print(type(sibling.tag))
+        print(type(sibling.attrib))
