9ff7644c1d60ffe0484d968f32f5427a5ab79923
zaricj <jovanzaric98@hotmail.com>
2024-02-22 11:50:17 +0100

    Update

--- xml_code/xmltree_tutorial.py
+++ xml_code/xmltree_tutorial.py
@@ -1,26 +1,40 @@
 import csv
 import xml.etree.ElementTree as ET
 
-datafile = "movies.xml"
+datafile = "Data.xml"
 
 tree = ET.parse(datafile)
 root = tree.getroot()
 
-root_tag = root.tag
-print(f"Root Tag: {root_tag}")
-root_attribute = root.attrib
-
-if root_attribute:
-    print(root_attribute)
-else:
-    print("Root has no attributes")
+def root_has_attributes():
+    root_tag = root.tag
+    print(f"Root Tag: {root_tag}")
+    root_attribute = root.attrib
     
-for child in root:
-    print(f"Child Tag: {child.tag} - Child Attribute: {child.attrib}")
-    for children in child:
-        print(f"Children Tag: {children.tag} - Children Attribute: {children.attrib}")
-        for sibling in children:
-            print(f"Sibling tag: {sibling.tag} - Sibling Attribute {sibling.attrib}")
+    if root_attribute:
+        print(root_attribute)
+    else:
+        print("Root has no attributes")
+    
+#for child in root:
+#    print(f"Child Tag: {child.tag} - Child Attribute: {child.attrib}")
+#    for subchild in child:
+#        print(f"Subchild Tag: {subchild.tag} - Subchild Attribute: {subchild.attrib}")
+#        for sibling in subchild:
+#            print(f"Sibling tag: {sibling.tag} - Sibling Attribute {sibling.attrib}")
+
+for element in root.iter("rank"): # Add Parameter in Function later for root.iter(parameter)
+    print(f"Tag: {element.tag}") # Add to Combo Element
+    print(f"Attribute: {element.attrib}") # Add to Combo Element
+    print(f"Value: {element.text}") # Add to Combo Element
+
+for country in root.findall('country'):
+    countryAtt = country.attrib
+    print(countryAtt)
+    print(country.get("name"))
+
+
+# Printing in a Text File for easier reading...
 
 #with open("text.txt", "w") as f:
 #    f.write(f"Root {root_tag} - {root_attribute}\n\n")
