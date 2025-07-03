7a34bce1e02506a195fb558498d154df21934ce2
Zaric, Jovan <jovanzaric98@hotmail.com>
2025-07-03 11:33:05 +0200

    Add search functionality to XML output text in QTextEdit

--- src/xmluvation/gui/controller.py
+++ src/xmluvation/gui/controller.py
@@ -1,7 +1,8 @@
 import csv
 import os
 import pandas as pd
-from PySide6.QtWidgets import QMessageBox, QComboBox, QRadioButton, QListWidget, QPushButton, QLineEdit, QMainWindow
+from PySide6.QtWidgets import QMessageBox, QComboBox, QRadioButton, QListWidget, QPushButton, QLineEdit, QTextEdit, QMainWindow
+from PySide6.QtGui import QTextDocument
 from xmluvation.modules.xpath_builder import create_xpath_validator, create_xpath_builder
 from xmluvation.modules.csv_export import create_csv_exporter
 from xmluvation.modules.file_cleanup import create_lobster_profile_cleaner, create_csv_column_dropper
@@ -539,7 +540,7 @@ class GenerateCSVHeaderHandler:
             return header
     
     @staticmethod 
-    def _is_duplicate(header:str, headers_list: str) -> bool:
+    def _is_duplicate(header :str, headers_list: str) -> bool:
         """Checks if the header is a duplicate. Prevents from adding the same header to the QLineEdit input for headers.
 
         Args:
@@ -551,3 +552,37 @@ class GenerateCSVHeaderHandler:
         
         return header in headers_list
 
+
+class SearchXMLOutputTextHandler:
+    """Handles methods and logic of the search functionality of the XML Output Text in the QTextEdit"""
+    def __init__(self,
+                main_window: QMainWindow,
+                line_edit_xml_output_find_text: QLineEdit,
+                text_edit_xml_output: QTextEdit
+                ):
+        
+        self.main_window = main_window
+        self.line_edit_xml_output_find_text = line_edit_xml_output_find_text
+        self.text_edit_xml_output = text_edit_xml_output
+
+    def search_next(self):
+        text = self.line_edit_xml_output_find_text.text()
+        try:
+            if text:
+                self.text_edit_xml_output.find(text)
+            else:
+                QMessageBox.information(self.main_window, "Find text input is empty", "No text has been entered for search criteria.")
+        except Exception as ex:
+            message = f"An exception of type {type(ex).__name__} occurred. Arguments: {ex.args!r}"
+            QMessageBox.critical(self.main_window, "Exception on XML search next function", message)
+
+    def search_previous(self):
+        text = self.line_edit_xml_output_find_text.text()
+        try:
+            if text:
+                self.text_edit_xml_output.find(text, QTextDocument.FindFlag.FindBackward)
+            else:
+                QMessageBox.information(self.main_window, "Find text input is empty", "No text has been entered for search criteria.")
+        except Exception as ex:
+            message = f"An exception of type {type(ex).__name__} occurred. Arguments: {ex.args!r}"
+            QMessageBox.critical(self.main_window, "Exception on XML search previous function", message)
