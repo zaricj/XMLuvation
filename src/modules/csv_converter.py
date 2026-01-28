79d598d6fb7b54d73bbc31b328767725b9c464f9
Zaric, Jovan <jovanzaric98@hotmail.com>
2026-01-28 14:57:39 +0100

    Fix: Excel Conversion

--- src/modules/csv_converter.py
+++ src/modules/csv_converter.py
@@ -5,31 +5,36 @@ import pandas as pd
 import csv
 import os
 
+
 class CSVConversionSignals(QObject):
     """Signals class for CSVConversionThread operations."""
-    
+
     error_occurred = Signal(str, str)  # QMessageBox.critical
     warning_occurred = Signal(str, str)  # QMessageBox.warning
     info_occurred = Signal(str, str)  # QMessageBox.information
-    tab2_program_output_append = Signal(str) # Append to CSV Output QTextEdit
-    set_file_open_path = Signal(str)  # Set the file path of the converted file in the "Open File" QLineEdit
+    tab2_program_output_append = Signal(str)  # Append to CSV Output QTextEdit
+    # Set the file path of the converted file in the "Open File" QLineEdit
+    set_file_open_path = Signal(str)
     start_gif = Signal()  # Tell Main Thread to start the GIF
     stop_gif = Signal()   # Tell Main Thread to stop the GIF
 
+
 class CSVConversionThread(QRunnable):
     """Handles methods and logic for csv_conversion_groupbox"""
+
     def __init__(self, operation: str, csv_file_to_convert: str, extension_type: str, write_index: bool, label_loading_gif: QLabel):
-        super().__init__() # Initialize QRunnable
+        super().__init__()  # Initialize QRunnable
         self.operation = operation
         # Operation parameters
         self.csv_file_to_convert = csv_file_to_convert
-        self.extension_type = extension_type # Value of the combobox self.ui.combobox_csv_conversion_output_type
+        # Value of the combobox self.ui.combobox_csv_conversion_output_type
+        self.extension_type = extension_type
         self.write_index = write_index
         self.label_loading_gif = label_loading_gif
-        
+
         self.signals = CSVConversionSignals()
         self.setAutoDelete(True)
-        
+
     @Slot()
     def run(self):
         try:
@@ -40,7 +45,7 @@ class CSVConversionThread(QRunnable):
                 raise ValueError(f"Unknown operation: {self.operation}")
         except Exception as e:
             self.signals.error_occurred.emit("Operation Error", f"{str(e)}")
-        
+
     def get_extension_type(self) -> str:
         """Returns the extension type for the conversion. """
         match self.extension_type:
@@ -60,40 +65,54 @@ class CSVConversionThread(QRunnable):
                 raise FileNotFoundError
             try:
                 # Detect delimiter
-                with open(self.csv_file_to_convert, newline="",encoding="utf-8") as file:
+                with open(self.csv_file_to_convert, newline="", encoding="utf-8") as file:
                     sample = file.read(2048)
                     sniffer = csv.Sniffer()
-                    delimiter = sniffer.sniff(sample).delimiter
-                    self.signals.tab2_program_output_append.emit(f"Detected delimiter: '{delimiter}'")
+                    # Limit delimiters to common ones and use quote handling to avoid confusion with quotes in text
+                    delimiter = sniffer.sniff(
+                        sample, delimiters=',;\t|', quotechar='"').delimiter
+                    self.signals.tab2_program_output_append.emit(
+                        f"Detected delimiter: '{delimiter}'")
             except Exception as e:
                 message = f"An error exception occurred while detecting the delimiter: {e}. Please ensure the file is a valid CSV."
-                self.signals.warning_occurred.emit("Delimiter Detection Error", message)
+                self.signals.warning_occurred.emit(
+                    "Delimiter Detection Error", message)
                 return
 
             # Load CSV
-            df = pd.read_csv(self.csv_file_to_convert, delimiter=delimiter, encoding="utf-8", engine="pyarrow")
+            df = pd.read_csv(self.csv_file_to_convert, delimiter=delimiter,
+                             encoding="utf-8", engine="pyarrow", quoting=csv.QUOTE_ALL)
 
             # Get extensions
             _, input_ext = os.path.splitext(self.csv_file_to_convert)
             output_ext = self.get_extension_type()
             input_ext = input_ext.lower().lstrip(".")
-            
+
             # Sheet name for Excel output
             if output_ext == "xlsx":
                 sheet_name = "Result"
-            
+
             # Define output file path in the same folder
             input_dir = os.path.dirname(self.csv_file_to_convert)
-            input_filename = os.path.splitext(os.path.basename(self.csv_file_to_convert))[0]
-            output_file_path = os.path.join(input_dir, input_filename + "." + output_ext)
-            
+            input_filename = os.path.splitext(
+                os.path.basename(self.csv_file_to_convert))[0]
+            output_file_path = os.path.join(
+                input_dir, input_filename + "." + output_ext)
+
             # Define conversion functions
-            def to_html(df, path): df.to_html(path, index=self.write_index)
-            def to_json(df, path): df.to_json(path, orient="records", force_ascii=False)
-            def to_md(df, path): df.to_markdown(path, index=self.write_index)
-            def to_xlsx(df, path):
+            def to_html(df: pd.DataFrame, path): df.to_html(
+                path, index=self.write_index)
+
+            def to_json(df: pd.DataFrame, path): df.to_json(
+                path, orient="records", force_ascii=False)
+
+            def to_md(df: pd.DataFrame, path): df.to_markdown(
+                path, index=self.write_index)
+
+            def to_xlsx(df: pd.DataFrame, path):
                 with pd.ExcelWriter(path, engine="xlsxwriter") as writer:
-                    df.to_excel(writer, sheet_name=sheet_name, index=self.write_index)
+                    df.to_excel(writer, sheet_name=sheet_name,
+                                index=self.write_index)
                     worksheet = writer.sheets[sheet_name]
                     max_row, max_col = df.shape
                     column_settings = [{"header": col} for col in df.columns]
@@ -113,35 +132,41 @@ class CSVConversionThread(QRunnable):
             }
 
             convert_func = conversion_map.get((input_ext, output_ext))
-            
+
             if not convert_func:
-                self.signals.error_occurred.emit("Unsupported Conversion", f"Cannot convert from '{input_ext}' to '{output_ext}'.")
+                self.signals.error_occurred.emit(
+                    "Unsupported Conversion", f"Cannot convert from '{input_ext}' to '{output_ext}'.")
                 return
 
             # Execute conversion
-            self.signals.tab2_program_output_append.emit("Starting conversion, please wait...")
-            
+            self.signals.tab2_program_output_append.emit(
+                "Starting conversion, please wait...")
+
             # Signal the main thread to START the GIF
             self.signals.start_gif.emit()
 
             # The conversion function will now run while the Main Thread is animating the GIF
-            convert_func(df, output_file_path) # Conversion runs here (can take a long time)
+            # Conversion runs here (can take a long time)
+            convert_func(df, output_file_path)
 
             # Signal the main thread to STOP the GIF
             self.signals.stop_gif.emit()
-                
-            self.signals.tab2_program_output_append.emit(f"Successfully converted:\n{os.path.basename(self.csv_file_to_convert)} → {os.path.basename(output_file_path)}")
-            
+
+            self.signals.tab2_program_output_append.emit(
+                f"Successfully converted:\n{os.path.basename(self.csv_file_to_convert)} → {os.path.basename(output_file_path)}")
+
             # Set the file path of the converted file in the "Open File" QLineEdit
             self.signals.set_file_open_path.emit(output_file_path)
 
         except FileNotFoundError:
-            self.signals.warning_occurred.emit("CSV Input File Error", "No CSV file has been selected for conversion.\nPlease select a CSV file.")
+            self.signals.warning_occurred.emit(
+                "CSV Input File Error", "No CSV file has been selected for conversion.\nPlease select a CSV file.")
 
         except Exception as ex:
             msg = f"{type(ex).__name__}: {ex}"
             self.signals.error_occurred.emit("Conversion Error", msg)
-            
+
+
 def create_csv_conversion_thread(operation: str, csv_file_to_convert: str, extension_type: str, write_index: bool, label_loading_gif: QLabel) -> CSVConversionThread:
     """Factory function to create a CSVConversionThread instance."""
     return CSVConversionThread(operation, csv_file_to_convert, extension_type, write_index, label_loading_gif)
